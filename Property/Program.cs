a6928206c6c77f62c9131f8273032c9c71807dc1
sosuisen <hidekazu.kubota@gmail.com>
2024-02-15 01:07:10 +0900

    fix: change order

--- Property/Program.cs
+++ Property/Program.cs
@@ -5,14 +5,15 @@ namespace Property
         static void Main(string[] args)
         {
             Order order1 = new Order(1, "Alan Kay", new DateTime(2022, 2, 2));
-            order1.DisplayOrder();
+            Order order2 = new Order(2, "Ted Nelson", new DateTime(2022, 3, 7));
 
-            Order order2 = new Order(2, "Ted Nelson", new DateTime(2022, 3, 7)); 
+            order1.DisplayOrder();
             order2.DisplayOrder();
 
             // Set a property
             order2.CustomerName = "Douglas Engelbart";
             order2.DisplayOrder();
+
             // Get a property
             Console.WriteLine(order2.OrderId);
         }
