09082f1e218e02fae600840011706a847b966206
sosuisen <hidekazu.kubota@gmail.com>
2024-02-15 02:56:53 +0900

    fix: simplify constructor

--- Property/Program.cs
+++ Property/Program.cs
@@ -4,8 +4,10 @@ namespace Property
     {
         static void Main(string[] args)
         {
-            Order order1 = new Order(1, "Alan Kay", new DateTime(2022, 2, 2));
-            Order order2 = new Order(2, "Ted Nelson", new DateTime(2022, 3, 7));
+            // Can use var
+            var order1 = new Order(1, "Alan Kay", new DateTime(2022, 2, 2));
+            // Can omit constructor name
+            Order order2 = new (2, "Ted Nelson", new DateTime(2022, 3, 7));
 
             order1.DisplayOrder();
             order2.DisplayOrder();
