e3b59523d055c6dc5fe610f9172e4fedcfc03cc6
sosuisen <hidekazu.kubota@gmail.com>
2024-02-14 22:43:00 +0900

    fix: change order1

--- Property/Program.cs
+++ Property/Program.cs
@@ -4,8 +4,8 @@ namespace Property
     {
         static void Main(string[] args)
         {
-            Order order = new Order(1, "Alan Kay", DateTime.Now);
-            order.DisplayOrder();
+            Order order1 = new Order(1, "Alan Kay", DateTime.Now);
+            order1.DisplayOrder();
 
             Order order2 = new Order(2, "Ted Nelson", DateTime.Now.AddDays(1));
             order2.DisplayOrder();
