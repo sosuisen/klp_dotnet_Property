f955b72fa97c6150fd4d503ac8addf3a7abb6c4b
sosuisen <hidekazu.kubota@gmail.com>
2024-02-21 16:24:27 +0900

    fix: use auto implemented properties

--- Property/ToDo.cs
+++ Property/ToDo.cs
@@ -2,9 +2,9 @@ namespace Syntax
 {
     internal class ToDo(int id, string name, DateTime deadline, bool completed)
     {
-        public int Id { get; } = id;
-        public string Name = name;
-        public DateTime Deadline = deadline;
-        public bool Completed = completed;
+        public int Id { get; set; } = id;
+        public string Name { get; set; } = name;
+        public DateTime Deadline { get; set; } = deadline;
+        public bool Completed { get; set; } = completed;
     }
 }
